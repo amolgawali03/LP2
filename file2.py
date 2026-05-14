7cfbeba8623e5ec73e1b220b0492f370ce770298
Amol Gawali <amolgawali122004@gmail.com>
2026-05-14 11:13:01 +0530

    Add files via upload

---
+++ file2.py
@@ -0,0 +1,10 @@
+n1=int(input("enter the first value :"))
+n2=int(input("enter the second value :"))
+
+print(f"before swapping :\n n1={n1} \n n2={n2}")
+
+n3=n1
+n1=n2
+n2=n3
+
+print(f"after swapping :\n n1={n1} \n n2={n2}")
